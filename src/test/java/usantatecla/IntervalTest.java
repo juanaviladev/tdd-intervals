d706006f509d14486301a066b909a3106a3d9e77
Juan Avila <dev@juanavila.es>
2020-11-29 21:04:38 +0100

    ♻️ Improve readability

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -83,21 +83,37 @@ public class IntervalTest {
     private static Stream<Arguments> intersectionTestCasesProvider() {
         return Stream.of(
                 Arguments.of("--{--}-------", "--------{--}-", builder().indifferent(left.getEquals()).indifferent(right.getLess()), builder().indifferent(right.getGreater()).indifferent(right.getGreater()), false),
+
                 Arguments.of("--(---)------", "--(---]------", builder().open(left.getEquals()).open(right.getEquals()), builder().open(left.getEquals()).closed(right.getEquals()), true),
+
                 Arguments.of("---{--)------", "-{----)------", builder().indifferent(left.getEquals()).open(right.getEquals()), builder().indifferent(left.getLess()).open(right.getEquals()), true),
+
                 Arguments.of("-----{---}---","---{-------}-", builder().indifferent(left.getGreater()).indifferent(right.getLess()), builder().indifferent(left.getEquals()).indifferent(right.getEquals()), true),
+
                 Arguments.of("---(------}--","---(----}----", builder().open(left.getEquals()).indifferent(right.getEquals()), builder().open(left.getEquals()).indifferent(right.getLess()), true),
+
                 Arguments.of("--(------]---","--(------]---", builder().open(left.getEquals()).closed(right.getEquals()), builder().open(left.getEquals()).closed(right.getEquals()), true),
+
                 Arguments.of("----{----}---","--{---}------", builder().indifferent(left.getGreater()).indifferent(right.getGreater()), builder().indifferent(left.getEquals()).indifferent(right.getEquals()), true),
+
                 Arguments.of("----{----]---","--{------]---", builder().indifferent(left.getGreater()).closed(right.getEquals()), builder().indifferent(left.getLess()).closed(right.getEquals()), true),
+
                 Arguments.of("-{-----)----","---{---)----", builder().indifferent(left.getLess()).open(right.getEquals()), builder().indifferent(left.getEquals()).open(right.getEquals()), true),
+
                 Arguments.of("-{-----}----","---{-----}--", builder().indifferent(left.getLess()).indifferent(right.getEquals()), builder().indifferent(left.getGreater()).indifferent(right.getGreater()), true),
+
                 Arguments.of("-[-----)----","-[-----)----", builder().closed(left.getEquals()).open(right.getEquals()), builder().closed(left.getEquals()).open(right.getEquals()), true),
+
                 Arguments.of("-[---}----","-[-----}----", builder().closed(left.getEquals()).indifferent(right.getLess()), builder().closed(left.getEquals()).indifferent(right.getEquals()), true),
+
                 Arguments.of("-{-------}--","---{---}----", builder().indifferent(left.getEquals()).indifferent(right.getEquals()), builder().indifferent(left.getLess()).indifferent(right.getLess()), true),
+
                 Arguments.of("-{-------]--","---{-----]--", builder().indifferent(left.getEquals()).closed(right.getEquals()), builder().indifferent(left.getGreater()).closed(right.getEquals()), true),
+
                 Arguments.of("-[-------}--","-[-----}----", builder().closed(left.getEquals()).indifferent(right.getGreater()), builder().closed(left.getEquals()).indifferent(right.getEquals()), true),
+
                 Arguments.of("-[-----]----","-[-----]----", builder().closed(left.getEquals()).closed(right.getEquals()), builder().closed(left.getEquals()).closed(right.getEquals()), true),
+                
                 Arguments.of("-(-----)----","-(-----)----", builder().open(left.getEquals()).open(right.getEquals()), builder().open(left.getEquals()).open(right.getEquals()), false)
         );
     }
