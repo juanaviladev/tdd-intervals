ca7900bf66990ea6d486a54ede5b375f7d9f5e37
Juan Avila <dev@juanavila.es>
2020-11-29 20:07:39 +0100

    ♻️ Refactor to parametrized

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -1,116 +1,128 @@
 package usantatecla;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IntervalTest {
-  
-  private Point left = new Point(-2.2);
-  private Point right = new Point(4.4);
 
-  @BeforeEach
-  public void before(){
-    this.left = new Point(-2.2);
-    this.right = new Point(4.4);
-  }
+    private static Point left = pointAt(-2.2);
+    private static Point right = pointAt(4.4);
 
-  @Test
-  public void givenIntervaOpenOpenlwhenIncludeWithIncludedValueThenTrue() {
-    Interval interval = builder().open(left.getEquals()).open(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertFalse(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
-    assertTrue(interval.include(right.getLess()));
-    assertFalse(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+    private static Stream<Arguments> intersectionTestCasesProvider() {
+        return Stream.of(
+                Arguments.of("--{--}-------", "--------{--}-", builder().indifferent(left.getEquals()).indifferent(right.getLess()), builder().indifferent(right.getGreater()).indifferent(right.getGreater()), false)
+        );
+    }
 
-  @Test
-  public void givenIntervaOpenOpenlwhenInc3ludeWithIncludedValueThenTrue() {
-    Interval interval = builder().closed(left.getEquals()).open(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertTrue(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
+    @Test
+    public void givenIntervaOpenOpenlwhenIncludeWithIncludedValueThenTrue() {
+        Interval interval = builder().open(left.getEquals()).open(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertFalse(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
+        assertTrue(interval.include(right.getLess()));
+        assertFalse(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-    assertTrue(interval.include(right.getLess()));
-    assertFalse(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+    @Test
+    public void givenIntervaOpenOpenlwhenInc3ludeWithIncludedValueThenTrue() {
+        Interval interval = builder().closed(left.getEquals()).open(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertTrue(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
 
-  @Test
-  public void givenIntervaOpenOpenlwhenIncludeWit3hIncludedValueThenTrue() {
-    Interval interval = builder().open(left.getEquals()).closed(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertFalse(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
+        assertTrue(interval.include(right.getLess()));
+        assertFalse(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-    assertTrue(interval.include(right.getLess()));
-    assertTrue(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+    @Test
+    public void givenIntervaOpenOpenlwhenIncludeWit3hIncludedValueThenTrue() {
+        Interval interval = builder().open(left.getEquals()).closed(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertFalse(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
 
-  @Test
-  public void givenIntervaOpenOpenlwhenIncludeWithInclude5dValueThenTrue() {
-    Interval interval = builder().closed(left.getEquals()).closed(right.getEquals()).build();
-    assertFalse(interval.include(left.getLess()));
-    assertTrue(interval.include(left.getEquals()));
-    assertTrue(interval.include(left.getGreater()));
+        assertTrue(interval.include(right.getLess()));
+        assertTrue(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-    assertTrue(interval.include(right.getLess()));
-    assertTrue(interval.include(right.getEquals()));
-    assertFalse(interval.include(right.getGreater()));
-  }
+    @Test
+    public void givenIntervaOpenOpenlwhenIncludeWithInclude5dValueThenTrue() {
+        Interval interval = builder().closed(left.getEquals()).closed(right.getEquals()).build();
+        assertFalse(interval.include(left.getLess()));
+        assertTrue(interval.include(left.getEquals()));
+        assertTrue(interval.include(left.getGreater()));
 
-  @Test
-  public void givenIntervalWhenIntersectWithNullIntervalThenError() {
-    Interval interval = builder().closed(left.getEquals()).closed(right.getEquals()).build();
-    assertThrows(AssertionError.class, () -> interval.isIntersected(null));
-  }
+        assertTrue(interval.include(right.getLess()));
+        assertTrue(interval.include(right.getEquals()));
+        assertFalse(interval.include(right.getGreater()));
+    }
 
-  @Test
-  public void givenIntervalWhenIntersectWithNonEnclosedIntervalThenFalse() {
-    Interval interval = builder().indifferent(left.getEquals()).indifferent(right.getLess()).build();
-    Interval nonEnclosed = builder().indifferent(right.getGreater()).indifferent(right.getGreater()).build();
+    @Test
+    public void givenIntervalWhenIntersectWithNullIntervalThenError() {
+        Interval interval = builder().closed(left.getEquals()).closed(right.getEquals()).build();
+        assertThrows(AssertionError.class, () -> interval.isIntersected(null));
+    }
 
-    assertFalse(interval.isIntersected(nonEnclosed));
-  }
+    @Test
+    public void givenOpenIntervalWhenIntersectWithOpenClosedIntervalThenTrue() {
+        Interval openInterval = builder().open(left.getEquals()).open(right.getEquals()).build();
+        Interval openClosed = builder().open(left.getEquals()).closed(right.getEquals()).build();
 
-  @Test
-  public void givenOpenIntervalWhenIntersectWithOpenClosedIntervalThenTrue() {
-    Interval openInterval = builder().open(left.getEquals()).open(right.getEquals()).build();
-    Interval openClosed = builder().open(left.getEquals()).closed(right.getEquals()).build();
+        assertTrue(openInterval.isIntersected(openClosed));
+    }
 
-    assertTrue(openInterval.isIntersected(openClosed));
-  }
+    @Test
+    public void givenLeftIndifferentOpenIntervalWhenIntersectWithOpenClosedIntervalThenTrue() {
+        Interval indifferentOpen = builder().indifferent(left.getEquals()).open(right.getEquals()).build();
+        Interval openClosed = builder().indifferent(left.getLess()).open(right.getEquals()).build();
 
-  @Test
-  public void givenLeftIndifferentOpenIntervalWhenIntersectWithOpenClosedIntervalThenTrue() {
-    Interval indifferentOpen = builder().indifferent(left.getEquals()).open(right.getEquals()).build();
-    Interval openClosed = builder().indifferent(left.getLess()).open(right.getEquals()).build();
+        assertTrue(indifferentOpen.isIntersected(openClosed));
+    }
 
-    assertTrue(indifferentOpen.isIntersected(openClosed));
-  }
+    @Test
+    public void givenIndifferentIntervalWhenIntersectWithIndifferentIntervalThenTrue() {
+        Interval indifferent = builder().indifferent(left.getGreater()).indifferent(right.getLess()).build();
+        Interval indifferentEncloser = builder().indifferent(left.getEquals()).indifferent(right.getEquals()).build();
 
-  @Test
-  public void givenIndifferentIntervalWhenIntersectWithIndifferentIntervalThenTrue() {
-    Interval indifferent = builder().indifferent(left.getGreater()).indifferent(right.getLess()).build();
-    Interval indifferentEncloser = builder().indifferent(left.getEquals()).indifferent(right.getEquals()).build();
+        assertTrue(indifferent.isIntersected(indifferentEncloser));
+    }
 
-    assertTrue(indifferent.isIntersected(indifferentEncloser));
-  }
+    @Test
+    public void givenOpenIndifferentIntervalWhenIntersectWithOpenIndifferentIntervalThenTrue() {
+        Interval openIndifferent = builder().open(left.getEquals()).indifferent(right.getEquals()).build();
+        Interval otherOpenIndifferent = builder().open(left.getEquals()).indifferent(right.getLess()).build();
 
-  @Test
-  public void givenOpenIndifferentIntervalWhenIntersectWithOpenIndifferentIntervalThenTrue() {
-    Interval openIndifferent = builder().open(left.getEquals()).indifferent(right.getEquals()).build();
-    Interval otherOpenIndifferent = builder().open(left.getEquals()).indifferent(right.getLess()).build();
+        assertTrue(openIndifferent.isIntersected(otherOpenIndifferent));
+    }
 
-    assertTrue(openIndifferent.isIntersected(otherOpenIndifferent));
-  }
+    @ParameterizedTest(name = "#{index} - Test with String : {0} vs. {1}")
+    @MethodSource("intersectionTestCasesProvider")
+    void test_method_string(String pivot, String other, IntervalBuilder pivotBuilder, IntervalBuilder otherBuilder, boolean expectedResult) {
+        Interval pivotInterval = pivotBuilder.build();
+        Interval otherInterval = otherBuilder.build();
 
-  private static IntervalBuilder builder() {
-    return new IntervalBuilder();
-  }
+        assertThat(pivotInterval.isIntersected(otherInterval), CoreMatchers.is(expectedResult));
+    }
+
+
+    private static IntervalBuilder builder() {
+        return new IntervalBuilder();
+    }
+
+    private static Point pointAt(double p) {
+        return new Point(p);
+    }
 
 }
